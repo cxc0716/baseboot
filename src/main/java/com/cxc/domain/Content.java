b19a1af0e193e6884ff144d1729667513a790461
cxc0716 <cxc0716@gmail.com>
2017-06-11 12:05:46 +0800

    content

---
+++ src/main/java/com/cxc/domain/Content.java
@@ -0,0 +1,13 @@
+package com.cxc.domain;
+
+import java.io.Serializable;
+
+/**
+ * author:chenxinchao
+ * date:2017-06-11 12:01
+ * desc:com.cxc.domain
+ */
+public class Content implements Serializable {
+
+	private long id;
+}
