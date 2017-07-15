4a4a78ec8ccd2ba263d69a9ffebce27c5565cda2
hzchenxinchao <hzchenxinchao@corp.netease.com>
2017-07-15 10:12:06 +0800

    log redirect url change

--- src/main/java/com/cxc/model/weixin/SendMsgResponse.java
+++ src/main/java/com/cxc/model/weixin/SendMsgResponse.java
@@ -14,6 +14,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class SendMsgResponse {
     @JsonProperty(value = "BaseResponse")
     private BaseResponse baseResponse;
+    @JsonProperty(value = "MsgID")
+    private String msgID;
+    @JsonProperty(value = "LocalID")
+    private String localID;
 
     public BaseResponse getBaseResponse() {
         return baseResponse;
@@ -21,5 +25,21 @@ public class SendMsgResponse {
 
     public void setBaseResponse(BaseResponse baseResponse) {
         this.baseResponse = baseResponse;
+    }
+
+    public String getMsgID() {
+        return msgID;
+    }
+
+    public void setMsgID(String msgID) {
+        this.msgID = msgID;
+    }
+
+    public String getLocalID() {
+        return localID;
+    }
+
+    public void setLocalID(String localID) {
+        this.localID = localID;
     }
 }
