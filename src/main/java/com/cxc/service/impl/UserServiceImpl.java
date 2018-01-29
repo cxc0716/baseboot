7d1b5e368c8b055b722a4362ac99107679e7acd6
hzchenxinchao <hzchenxinchao@corp.netease.com>
2018-01-29 19:18:40 +0800

    init: init the project

--- src/main/java/com/cxc/service/impl/UserServiceImpl.java
+++ src/main/java/com/cxc/service/impl/UserServiceImpl.java
@@ -1,20 +1,11 @@
 package com.cxc.service.impl;
 
-import java.io.File;
-import java.util.Date;
-import java.util.Locale;
-
-import com.cxc.common.util.HttpClientTemplate;
-import com.cxc.domain.Content;
-import com.cxc.domain.HiUser;
-import com.cxc.mapper.UserMapper;
-import com.cxc.model.QrcodeInfo;
-import com.cxc.service.UserService;
-
-import org.apache.commons.lang.time.DateFormatUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
+import com.cxc.domain.HiUser;
+import com.cxc.mapper.UserMapper;
+import com.cxc.service.UserService;
 
 /**
  * author:chenxinchao date:2016-09-22 15:55 desc:com.cxc.service.impl
